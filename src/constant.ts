457f02f1ba3d5e231bbe13675faac38d511bb4a3
Almaz Yakhin <vochko50@gmail.com>
2021-10-14 15:43:17 +0300

    [master] запросы в статике заработали, нужно делать запрос в https

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
-export const URL = "http://www.omdbapi.com/";
+export const URL = "https://www.omdbapi.com/";
 // export const URL_FULL_DATA: string = "http://www.omdbapi.com/";
 
 export const API_KEY = "&apikey=342c4fd5";
